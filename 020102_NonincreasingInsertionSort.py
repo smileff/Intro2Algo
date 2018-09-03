632b03d6fcb0a395759317bdcdbeff0439be52d3
yue <xie_8628@hotmail.com>
2018-09-03 23:00:49 +0800

    * Add exercise 2.1-2.

---
+++ 020102_NonincreasingInsertionSort.py
@@ -0,0 +1,15 @@
+def NonincreasingInsertionSort(nums):
+    print("Input numbers: ", nums)
+    for j in range(1, len(nums)):
+        tmp = nums[j]
+        i = j
+        while i > 0 and nums[i - 1] < tmp:
+            nums[i] = nums[i - 1]
+            i = i - 1
+        nums[i] = tmp
+        print("After {} iteration: ".format(j - 1), nums)
+    return nums
+
+if __name__ == "__main__":
+    data = [1, 2, 3, 4, 5]
+    print(NonincreasingInsertionSort(data))
